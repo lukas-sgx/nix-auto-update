79cd8a0437d398e583f5728d88fbd66ca7143d7d
lukas-sgx <lukas.soigneux@epitech.eu>
2026-08-14 19:49:30 +0200

    chore: fmt

--- nix_auto_update/cli.py
+++ nix_auto_update/cli.py
@@ -1,6 +1,7 @@
 import os
 import git
 
+
 def clone_repo(remote_url, target_dir):
     try:
         if os.path.exists(target_dir):
@@ -9,15 +10,16 @@ def clone_repo(remote_url, target_dir):
             if repo.bare:
                 print("Warning: The local repository is bare.")
             return repo
-        
+
         print(f"Cloning {remote_url} into '{target_dir}'...")
         repo = git.Repo.clone_from(remote_url, to_path=target_dir, depth=1)
         return repo
 
-    except (git.exc.GitCommandError) as e:
+    except git.exc.GitCommandError as e:
         print(e)
-    except (git.exc.InvalidGitRepositoryError) as e:
+    except git.exc.InvalidGitRepositoryError as e:
         print(e)
+
 
 def main():
     clone_repo("git@github.com:lukas-sgx/nixpkgs.git", "nixpkgs")
